7eafc96ada1df70beb48fc1440b576d7116510e4
RushabhS-RSW <rushabh@rainstreamweb.com>
2022-09-12 10:54:31 +0530

    add a afs function name in terraform

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -10,22 +10,22 @@ const Menu = () =>{
         <>
             <div className="nav">
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/"> Axios </NavLink>  
+                <NavLink exact activeClassName={activeClass} to="/"> Axios </NavLink>  
                 </div>
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/boot"> boot </NavLink>
+                <NavLink exact activeClassName={activeClass} to="/boot"> boot </NavLink>
                 </div>
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/asiox"> card </NavLink>
+                <NavLink exact activeClassName={activeClass} to="/asiox"> card </NavLink>
                 </div>
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/search"> Search </NavLink>
+                <NavLink exact activeClassName={activeClass} to="/search"> Search </NavLink>
                 </div>
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/Weather"> Weather </NavLink>
+                <NavLink exact activeClassName={activeClass} to="/Weather"> Weather </NavLink>
                 </div>
                 <div className="nav_manu">
-                <NavLink exact activeClassName={activeClass} to="/reacttut/api"> Api </NavLink>
+                <NavLink exact activeClassName={activeClass} to="/api"> Api </NavLink>
                 </div>
             </div>
         </>
